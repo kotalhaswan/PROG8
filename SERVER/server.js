3b7c9515200f6ee6d6afcbde7fbd71487a30d070
aswankotalh <74552374+aswankotalh@users.noreply.github.com>
2024-02-29 15:19:53 +0100

    start again 3

---
+++ SERVER/server.js
@@ -0,0 +1,11 @@
+import { ChatOpenAI } from "@langchain/openai";
+
+const model = new ChatOpenAI({
+    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+    azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
+    azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
+})
+
+const joke = await model.invoke("Tell me a Javascript joke!")
+console.log(joke.content)
